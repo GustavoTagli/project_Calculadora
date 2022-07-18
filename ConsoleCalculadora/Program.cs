90b57b44be0ebe65b5228d7c57951f11ed82af89
Gustavo Taglianetti <taglisouza.0304@hotmail.com>
2022-07-18 17:19:27 -0300

    @GustavoTagli[exercise_calculadora]

---
+++ ConsoleCalculadora/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace ConsoleCalculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcao = 0, val1=0, val2=0;
+            Console.WriteLine("Pressione qualquer tecla para ligar a calculadora");
+            Console.ReadKey();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("====== MENU ======");
+                Console.WriteLine("==================");
+                Console.WriteLine("[1] Somar");
+                Console.WriteLine("[2] Subtrair");
+                Console.WriteLine("[3] Multiplicar");
+                Console.WriteLine("[4] Dividir");
+                Console.WriteLine("[5] Desligar");
+                try
+                {
+                    opcao = int.Parse(Console.ReadLine());
+                    if (opcao == 5) break;
+                    if (opcao > 5)
+                    {
+                        Erros.Invalido();
+                        continue;
+                    }
+                }
+                catch
+                {
+                    Erros.Invalido();
+                    continue;
+                }
+
+
+                Console.Clear();
+                Console.WriteLine("Insira o 1º valor:");
+                try { val1 = int.Parse(Console.ReadLine()); }
+                catch 
+                {
+                    Erros.valorNumerico();
+                    continue;
+                }
+                Console.WriteLine("Insira o 2º valor");
+                try { val2 = int.Parse(Console.ReadLine()); }
+                catch
+                {
+                    Erros.valorNumerico();
+                    continue;
+                }
+
+                switch (opcao)
+                {
+                    case 1:
+                        Somar(val1, val2);
+                        break;
+
+                    case 2:
+                        Subtrair(val1, val2);
+                        break;
+
+                    case 3:
+                        Multiplicar(val1, val2);
+                        break;
+
+                    case 4:
+                        Dividir(val1, val2);
+                        break;
+                }
+            }
+        }
+
+        public static void Somar(int n1, int n2)
+        {
+            Console.WriteLine($"\n{n1} + {n2} = {n1 + n2}");
+            Console.ReadKey();
+        }
+
+        public static void Subtrair(int n1, int n2)
+        {
+            Console.WriteLine($"\n{n1} - {n2} = {n1 - n2}");
+            Console.ReadKey();
+        }
+
+        public static void Multiplicar(int n1, int n2)
+        {
+            Console.WriteLine($"\n{n1} x {n2} = {n1 * n2}");
+            Console.ReadKey();
+        }
+
+        public static void Dividir(float n1, float n2)
+        {
+            Console.WriteLine($"\n{n1} ÷ {n2} = {n1 / n2}");
+            Console.ReadKey();
+        }
+
+        class Erros
+        {
+            public static void Invalido()
+            {
+                Console.WriteLine("Opção inválida! Digite novamente.");
+                Console.ReadKey();
+            }
+
+            public static void valorNumerico()
+            {
+                Console.WriteLine("ERRO: digite um número para que o programe funcione");
+                Console.ReadKey();
+            }
+        }
+    }
+}
